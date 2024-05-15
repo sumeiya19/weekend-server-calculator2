3f6015532511efce60007d80baf6d793613350eb
Sumeiya Abdi <sumeiya619@gmail.com>
2024-05-14 20:57:42 -0500

    worked on GET and POST

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,66 @@
-console.log('client.js is sourced!');
+let currentOperator;
+
+// Takes an operator directly
+let selectOperator = (op) => {
+    currentOperator = op;
+    console.log("current operator is...", currentOperator);
+};
+
+function getCalculations(){
+    console.log("getCalculations is working...");
+    //Axios
+    axios({
+        method: "GET",
+        url: "/calculations"
+    })
+    .then((response) => {
+        console.log("response.data from /calculations:", response.data);
+    })
+    .catch((error) => {
+        console.error("There was an error on GET /calculations", error)
+    })
+}
+
+getCalculations()
+
+// POST route
+
+function postCalculations(event){
+    event.preventDefault()
+    console.log("New postCalculations created...");
+
+    let numOne = document.getElementById("numOne").value
+    let numTwo = document.getElementById("numTwo").value
+
+    let newCalculations = {
+        num1: numOne,
+        num2: numTwo, 
+        operator: currentOperator
+    }
+    console.log("New calculations to send...", newCalculations);
+
+    // Axios POST request
+    axios({
+        method: "POST",
+        url: "/calculations",
+        data: newCalculations
+    })
+    .then((response) => {
+        console.log("POST /calculations works!!");
+
+        getCalculations()
+    })
+    .catch((error) => {
+        console.error("There was an error on POST /calculations", error)
+            alert("POST /calculations didn't work...")
+    })
+}
+
+//Render function to display history on the DOM
+
+    function renderHistory(calculations){
+        console.log("renderHistory() works", calculations);
+    }
+    renderHistory()
+
+
--- server/server.js
+++ server/server.js
@@ -3,19 +3,77 @@ const app = express();
 let PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('server/public'));
 
 // Global variable that will contain all of the
 // calculation objects:
-let calculations = []
+let calculations = [{
+  numOne: 2,
+  operator: "+",
+  numTwo: 4,
+  result: 6
+},
+{
+  numOne: 6,
+  operator: "*",
+  numTwo: 6,
+  result: 36
+},
+{
+numOne: 4,
+operator: "-",
+numTwo: 1,
+result: 3
+}
+];
 
 
 // Here's a wonderful place to make some routes:
 
 // GET /calculations
+app.get('/calculations', (req, res) => {
+  res.send(calculations)
+})
+
 
 // POST /calculations
+app.post('/calculations', (req, res) => {
+  let newHistory = req.body
+  newHistory.num1 = Number(newHistory.num1)
+  newHistory.num2 = Number(newHistory.num2)
 
+  let result = calcResult(newHistory)
+  console.log("Result is... ", result)
+
+  calculations.push(newHistory)
+
+  console.log("POST /calculations working:", newHistory);
+  res.sendStatus(200)
+})
+
+
+
+
+let calcResult = (toCalculate) => {
+  console.log("calcResult() called from /postHistory");
+  
+  let num1 = toCalculate.num1;
+  let num2 = toCalculate.num2;
+
+  // Perform the calculation based on the operator
+  if (toCalculate.operator === "+") {
+    return num1 + num2;
+  } else if (toCalculate.operator === "-") {
+    return num1 - num2;
+  } else if (toCalculate.operator === "*") {
+    return num1 * num2;
+  } else if (toCalculate.operator === "/") {
+    return num1 / num2;
+  } else {
+    return NaN; // Return NaN for unsupported operators
+  }
+};
 
 // PLEASE DO NOT MODIFY ANY CODE BELOW THESE BEARS:
 // 🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸
